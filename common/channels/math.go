2b9aa4852af77f79e74841c124003d730692d0be
Chris Smith <chris@chameth.com>
2023-12-05 12:46:54 +0000

    Day 5

--- common/channels/math.go
+++ common/channels/math.go
@@ -1,5 +1,7 @@
 package channels
 
+import "math"
+
 // Sum adds up all the values received on the channel
 func Sum(channel <-chan int) chan int {
 	res := make(chan int)
@@ -10,6 +12,22 @@ func Sum(channel <-chan int) chan int {
 			sum += o
 		}
 		res <- sum
+	}()
+	return res
+}
+
+// Min finds the smallest value received on the channel
+func Min(channel <-chan int) chan int {
+	res := make(chan int)
+	go func() {
+		defer close(res)
+		lowest := math.MaxInt64
+		for o := range channel {
+			if o < lowest {
+				lowest = o
+			}
+		}
+		res <- lowest
 	}()
 	return res
 }
