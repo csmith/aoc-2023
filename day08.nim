9f9ea1e0764e24ee62ddc885a69c5bf382e029f8
Chris Smith <chris@chameth.com>
2023-12-10 02:37:56 +0000

    Some day 8 optimisations
    Do horrible things to the IDs so we can allocate less memory

--- day08.nim
+++ day08.nim
@@ -2,9 +2,48 @@ import math, memfiles, sequtils
 
 const identifierSize = uint(sizeof(uint16))
 
+const z: uint16 = 20
+const zzz: uint16 = ((z*20+z) shl 5) or z
+
+# Not all letters seem to be used, so we can squish the identifiers a bit to
+# save allocating as much memory. Can't get down to 4 bits, though :(
+const idMap: seq[uint16] = @[
+  0,  # A
+  1,  # B
+  2,  # C
+  3,  # D
+  0,  # 
+  4,  # F
+  5,  # G
+  6,  # H
+  0,  #
+  7,  # J
+  8,  # K
+  9,  # L
+  10, # M
+  11, # N
+  0,  #
+  12, # P
+  13, # Q
+  14, # R
+  15, # S
+  16, # T
+  0,  # 
+  18, # V
+  0,  # 
+  19, # X
+  0,  #
+  20, # Z
+]
+
 func identifier(s: cstring, offset: int): uint16 {.inline.} =
-  for i in 0..2:
-    result = (result shl 5) or uint16(s[offset+i].ord - 'A'.ord)
+  # We want the last letter to be in the low 5 bits so we can check
+  # it using a mask, but we can compact the other two letters together.
+  let
+    a = idMap[uint16(s[offset].ord - 'A'.ord)]
+    b = idMap[uint16(s[offset+1].ord - 'A'.ord)]
+    c = idMap[uint16(s[offset+2].ord - 'A'.ord)]
+  result = ((a*20+b) shl 5) or c
 
 func writeDirections(map: pointer, source: uint16, left: uint16, right: uint16): void {.inline.} =
   let a = cast[uint](map) + source*2*identifierSize
@@ -26,7 +65,7 @@ func countSteps(map: pointer, source: uint16, targetMask: uint16, directions: se
 
 proc main(): void =
   let input = memfiles.open("inputs/08.txt")
-  let map = alloc(identifierSize*26425*2)
+  let map = alloc(zzz*identifierSize*2)
  
   var first = true
   var directions: seq[bool]
@@ -43,12 +82,12 @@ proc main(): void =
       let source = identifier(line, 0)
       writeDirections(map, source, identifier(line, 7), identifier(line, 12))
       if (source and 0b11111) == 0:
-        starts.add(identifier(line, 0))
+        starts.add(source)
 
   # Part one: run until we get to a ZZZ node
-  echo countSteps(map, 0, 0b110011100111001, directions)
+  echo countSteps(map, 0, zzz, directions)
   # Part two: run each *A node until we get to a *Z node, then LCM the results
   # This happens to work on all inputs for $reasons, despite not being specified or hinted at...
-  echo math.lcm(starts.mapIt(countSteps(map, it, 0b11001, directions)))
+  echo math.lcm(starts.mapIt(countSteps(map, it, z, directions)))
 
 main()
