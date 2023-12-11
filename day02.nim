ad0e2b252d6772f36685d09e5e78096875edd7dc
Chris Smith <chris@chameth.com>
2023-12-11 23:55:09 +0000

    Day 2 minor optimisation: skip useless text

--- day02.nim
+++ day02.nim
@@ -14,22 +14,28 @@ proc main(): void =
 
   for slice in memSlices(input):
     let line = cast[cstring](slice.data)
-    for i in 0..slice.size:
+    var i = 5
+    while i < slice.size:
       let b = line[i]
       if b >= '0' and b <= '9':
         digit = 10 * digit + (b.ord - '0'.ord)
       elif b == ':':
         game = digit
         digit = 0
+        inc i
       elif b == 'r' and digit > 0:
         minRed = max(minRed, digit)
         digit = 0
+        i += 4
       elif b == 'g' and digit > 0:
         minGreen = max(minGreen, digit)
         digit = 0
+        i += 6
       elif b == 'b' and digit > 0:
         minBlue = max(minBlue, digit)
         digit = 0
+        i += 5
+      inc i
     
     if minRed <= 12 and minGreen <= 13 and minBlue <= 14:
       partOne += game
