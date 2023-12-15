c9d29c32bb601b731c6182861a73088d4982cd32
Chris Smith <chris@chameth.com>
2023-12-15 10:33:50 +0000

    Actually process the last instruction, I _guess_

--- day15.nim
+++ day15.nim
@@ -13,19 +13,22 @@ proc main(): void =
     lenses = newTable[int, int]()
     hashmap = newSeq[seq[int]](256)
 
+  proc process() =
+    partOne = partOne + hash
+    if value > 0:
+      lenses[label] = value
+      let pos = hashmap[labelHash].find(label)
+      if pos == -1:
+        hashmap[labelHash].add(label)
+    label = 0
+    hash = 0
+    value = 0
+
   for slice in memSlices(input):
     let line = cast[cstring](slice.data)
     for i in 0..slice.size-1:
       if line[i] == ',':
-        partOne = partOne + hash
-        if value > 0:
-          lenses[label] = value
-          let pos = hashmap[labelHash].find(label)
-          if pos == -1:
-            hashmap[labelHash].add(label)
-        label = 0
-        hash = 0
-        value = 0
+        process()
       else:
         if line[i] >= 'a' and line[i] <= 'z':
           label = label * 26 + (line[i].ord - 'a'.ord)
@@ -39,7 +42,7 @@ proc main(): void =
           value = 10 * value + (line[i].ord - '0'.ord)
         hash = (17 * (hash + line[i].ord)) mod 256
 
-    partOne = partOne + hash
+    process()
   
   echo partOne
 
